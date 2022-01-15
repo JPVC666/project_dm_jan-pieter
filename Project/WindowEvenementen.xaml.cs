c1b2bc068eddf8971f695f6cbdc05b0322453a7f
JPVC666 <r0840634@student.thomasmore.be>
2022-01-15 15:37:14 +0100

    edited something

--- Project/WindowEvenementen.xaml.cs
+++ Project/WindowEvenementen.xaml.cs
@@ -64,76 +64,6 @@ namespace Project
             Resetten();
         }
 
-        private void btnVoegEvenementToe_Click(object sender, RoutedEventArgs e)
-        {
-            string foutmelding = Valideer("txtTitel");
-            foutmelding += Valideer("txtStraat");
-            foutmelding += Valideer("txtGemeente");
-            foutmelding += Valideer("txtId");
-            foutmelding += Valideer("txtBeschrijving");
-            foutmelding += Valideer("txtPostcode");
-            foutmelding += Valideer("txtHuisnummer");
-
-            if (string.IsNullOrWhiteSpace(foutmelding))
-            {
-                Event ev = new Event();
-                ev.titel = txtTitel.Text;
-                ev.straat = txtStraat.Text;
-                ev.gemeente = txtGemeente.Text;
-                ev.id = int.Parse(txtId.Text);
-                ev.beschrijving = txtBeschrijving.Text;
-                ev.postcode = txtPostcode.Text;
-                ev.huisnr = txtHuisnummer.Text;
-
-                if (ev.IsGeldig())
-                {
-                    int ok = DatabaseOperations.ToevoegenEvent(ev);
-                    if (ok <= 0)
-                    {
-                        MessageBox.Show("Toevoegen van een evenement is niet gelukt");
-                    }
-                    else
-                    {
-                        datagridEvenementen.ItemsSource = DatabaseOperations.OphaleEventsViaTitels(ev.titel);
-                    }
-                }
-
-                Resetten();
-            }
-            else
-            {
-                MessageBox.Show(foutmelding);
-            }
-        }
-
-        private void btnVerwijderEvenement_Click(object sender, RoutedEventArgs e)
-        {
-            string foutmelding = Valideer("datagridEvenementen");
-
-            if (string.IsNullOrWhiteSpace(foutmelding))
-            {
-                //verwijderen
-                if (datagridEvenementen.SelectedItem is Event ev)
-                {
-                    //verwijderen uit database -> method toevoegen aan databaseOperations
-                    int ok = DatabaseOperations.VerwijderEvent(ev);
-                    if (ok <= 0)
-                    {
-                        MessageBox.Show("Verwijderen van een evenement is niet gelukt");
-                    }
-                    else
-                    {
-                        datagridEvenementen.ItemsSource = DatabaseOperations.OphaleEventsViaTitels(ev.titel);
-                        Resetten();
-                    }
-                }
-            }
-            else
-            {
-                MessageBox.Show(foutmelding);
-            }
-        }
-
         private string Valideer(string columnName)
         {
             if (columnName == "txtTitel" && string.IsNullOrWhiteSpace(txtTitel.Text))
@@ -171,6 +101,76 @@ namespace Project
             return "";
         }
 
+        private void btnVoegEvenementToe_Click(object sender, RoutedEventArgs e)
+        {
+            string foutmelding = Valideer("txtTitel");
+            foutmelding += Valideer("txtStraat");
+            foutmelding += Valideer("txtHuisnummer");
+            foutmelding += Valideer("txtId");
+            foutmelding += Valideer("txtGemeente");
+            foutmelding += Valideer("txtBeschrijving");
+            foutmelding += Valideer("txtPostcode");
+
+            if (string.IsNullOrWhiteSpace(foutmelding))
+            {
+                Event feest = new Event();
+                feest.titel = txtTitel.Text;
+                feest.straat = txtStraat.Text;
+                feest.huisnr = txtHuisnummer.Text;
+                feest.id = int.Parse(txtId.Text);
+                feest.gemeente = txtGemeente.Text;
+                feest.beschrijving = txtBeschrijving.Text;
+                feest.postcode = txtPostcode.Text;
+
+                if (feest.IsGeldig())
+                {
+                    int ok = DatabaseOperations.ToevoegenEvent(feest);
+                    if (ok <= 0)
+                    {
+                        MessageBox.Show("Toevoegen van een evenement is niet gelukt");
+                    }
+                    else
+                    {
+                        datagridEvenementen.ItemsSource = DatabaseOperations.OphaleEventsViaTitels(feest.titel);
+                    }
+                }
+
+                Resetten();
+            }
+            else
+            {
+                MessageBox.Show(foutmelding);
+            }
+        }    
+
+        private void btnVerwijderEvenement_Click(object sender, RoutedEventArgs e)
+        {
+            string foutmelding = Valideer("datagridEvenementen");
+
+            if (string.IsNullOrWhiteSpace(foutmelding))
+            {
+                //verwijderen
+                if (datagridEvenementen.SelectedItem is Event feest)
+                {
+                    //verwijderen uit database -> method toevoegen aan databaseOperations
+                    int ok = DatabaseOperations.VerwijderEvent(feest);
+                    if (ok <= 0)
+                    {
+                        MessageBox.Show("Verwijderen van een evenement is niet gelukt");
+                    }
+                    else
+                    {
+                        datagridEvenementen.ItemsSource = DatabaseOperations.OphaleEventsViaTitels(feest.titel);
+                        Resetten();
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show(foutmelding);
+            }
+        }
+       
         private void Resetten()
         {
             txtTitel.Text = "";
